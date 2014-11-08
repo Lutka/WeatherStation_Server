180ff03797cd42eb0fb73f68a95ed586c92ac2e1
Paulina Luty <plutka88@gmail.com>
2014-11-08 20:17:43 +0000

    add return of result code, to allow identifying if the data got inserted to db

---
+++ index.php
@@ -0,0 +1,4 @@
+<html><body><h1>Placeholder Weather Page</h1>
+<p>This is a placeholder for the "weather" host</p>
+<?php echo "Raspberry Pi Weather Station Project"?>
+</body></html>
--- input.php
+++ input.php
@@ -3,30 +3,36 @@
 
 //http://weather.cs.nuim.ie/input.php?device_id=1&time=1414532294&value=23&sensor_type=T
 
+define (rc_successful, 200);
+define (rc_bad_request, 404);
+define (rc_server_error, 500);
+
 //to prevent sql injection 
 //validation of the input
 if(!is_numeric($_REQUEST['device_id']))
 {
-  exit("Invalid device_id");
+  http_response_code(rc_bad_request);
+  exit("Invalid device_id");  
 }
 
 if(!is_numeric($_REQUEST['time']))
 {
+  http_response_code(rc_bad_request);
   exit("Invalid time");
 }
 
 if(!is_numeric($_REQUEST['value']))
 {
+  http_response_code(rc_bad_request);
   exit("Invalid value");
 }
 
-//echo preg_match('[A-Z]', $_REQUEST['sensor_type']);
-
 $sensor_type_arr=array("T","H");
-//in_array("Irix", $os)
 
 if(!in_array($_REQUEST['sensor_type'], $sensor_type_arr)) 
 {
+  http_response_code(rc_bad_request);
+  
   //('[A-Z]', $_REQUEST['sensor_type'])) {
   exit($_REQUEST['sensor_type']." is invalid sensor_type");
 }
@@ -50,15 +56,16 @@ or die (mysqli_connect_error());
 
  if($connect) {
     if(mysqli_query($connect, $sql_insert)) {
+	   http_response_code(rc_successful);
        echo "New record added";
     }
     else
     {
+	  http_response_code(rc_server_error);
       echo "Error: " . mysqli_error($connect);
     }
  }
-  mysqli_close($connect);
-   
+  mysqli_close($connect);   
 
 ?>
 
