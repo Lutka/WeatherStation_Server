0ad9b0ed35097b43721400f9375cfeeaf08faf58
Paulina Luty <plutka88@gmail.com>
2014-11-16 20:48:00 +0000

    change to save numerical values as int (used to be as strings)

--- output.php
+++ output.php
@@ -18,9 +18,23 @@ JOIN sensortype USING(typeID)";
 $result = mysqli_query($connect, $q_readings) 
 or die (mysqli_error($connect));
 
-$array = array();
+$array = array(); 
  while($row=mysqli_fetch_array($result, MYSQLI_ASSOC)) 
 { 
+	#cast in order to have values in json as integers, so without quotes
+	$row["SpecID"] = intval($row["SpecID"]); 
+	$row["SensorID"] = intval($row["SensorID"]); 
+	$row["LocationID"] = intval($row["LocationID"]);
+	$row["ReadingID"] = intval($row["ReadingID"]); 
+	$row["Time"] = intval($row["Time"]); 
+	$row["Value"] = intval($row["Value"]); 
+	$row["Latitude"] = intval($row["Latitude"]); 
+	$row["Longitude"] = intval($row["Longitude"]); 
+	$row["DeviceID"] = intval($row["DeviceID"]);
+	$row["Accuracy"] = intval($row["Accuracy"]); 
+	$row["Minimum"] = intval($row["Minimum"]);
+	$row["Maximum"] = intval($row["Maximum"]); 
+	
 	$array[] = $row;
 } 
 $feed["readings"] = $array;
