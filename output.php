0e543305dfc3f281005692ffac57439c871c0e00
Paulina Luty <plutka88@gmail.com>
2014-11-16 20:34:05 +0000

    create readings feed

--- output.php
+++ output.php
@@ -1,13 +1,4 @@
-<html>
-<head>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-</head>
-<html>
-<body>
 <?php
 
 #login details
@@ -18,37 +9,22 @@ $database = "weather";
 $connect = mysqli_connect("localhost", $username, $password, $database)
 or die (mysqli_connect_error());
 
-#echo ("<p>Connected to database.</p>");
-echo ("<p>Weather Station Readings</p>");
 
-$query = "SELECT * FROM reading ORDER BY time DESC LIMIT 100";  
-$result = mysqli_query($connect, $query) 
+$q_readings = "SELECT * FROM reading 
+JOIN location USING(locationID)
+JOIN sensor USING(sensorID)
+JOIN sensorspec USING(specID)
+JOIN sensortype USING(typeID)"; 
+$result = mysqli_query($connect, $q_readings) 
 or die (mysqli_error($connect));
 
-
-echo "<table class='table table-hover'>
-<thead>
-	<tr>
-		<th>ReadingID</th>
-		<th>SensorID</th>
-		<th>Time</th>
-		<th>Value</th>
-		<th>LocationID</th>
-	</tr>
-</thead>";
+$array = array();
  while($row=mysqli_fetch_array($result, MYSQLI_ASSOC)) 
 { 
-	$date=gmdate("Y-m-d\ | H:i:s\ ", $row[Time]);
-	echo"<tr> 
-	<td> $row[ReadingID] </td>
-	<td> $row[SensorID] </td>
-	<td> $date </td>   
-	<td> $row[Value] </td>
-	<td> $row[LocationID] </td> 
-	</tr>";	
+	$array[] = $row;
 } 
+$feed["readings"] = $array;
 
-echo "</table>";
+echo json_encode($feed);
+
 ?>
-<body>
-</html>
