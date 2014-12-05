4b04254a8ea09d433baf94ed53b89f1482804681
Paulina Luty <plutka88@gmail.com>
2014-12-05 22:30:21 +0000

    reformat of the readins from db, humidity and temerature on one row

--- index.php
+++ index.php
@@ -16,30 +16,75 @@ include("connect.php");
 #echo ("<p>Connected to database.</p>");
 echo ("<p>Weather Station Readings</p>");
 
-$query = "SELECT * FROM reading ORDER BY time DESC LIMIT 100";  
+$query = "SELECT time, value, reading.locationID, sensorSpec.typeID, device.deviceType, location.comment
+FROM reading
+JOIN sensor USING (sensorID) 
+JOIN device USING(deviceID)
+JOIN location ON(reading.locationID= location.locationID)
+JOIN sensorSpec USING (specID)
+JOIN sensorType USING (typeID)
+ORDER BY time DESC LIMIT 200";    
 $result = mysqli_query($connect, $query) 
 or die (mysqli_error($connect));
 
+$readings=array();
+$lastReading=null;
+
+ while($row=mysqli_fetch_array($result, MYSQLI_ASSOC)) 
+{	
+	if($lastReading != null)
+	{
+		if($lastReading["time"] == $row["time"] && $lastReading["deviceType"] == $row["deviceType"])
+		{
+			if($row[typeID] =='H')
+			{
+				$readings[sizeof($readings)-1]["humidity"] = $row["value"];
+			}
+			else
+			{
+				$readings[sizeof($readings)-1]["temperature"] = $row["value"]; 
+			}
+			continue;
+		}
+	} 
+	
+	
+	if($row[typeID] =='H')
+	{	
+		$row["humidity"]=$row["value"];
+	}	
+	else ##($row[typeID] =='T')
+	{
+		$row["temperature"] = $row["value"];
+	}
+	
+	$lastReading=$row;
+	$readings[]=$lastReading;
+}
 
 echo "<table class='table table-hover'>
 <thead>
 	<tr>
-		<th>ReadingID</th>
-		<th>SensorID</th>
 		<th>Time</th>
-		<th>Value</th>
-		<th>LocationID</th>
+		<th>Location</th>
+		<th>Temperature</th>
+		<th>Humidity</th>
+		<th>Type</th>
 	</tr>
 </thead>";
- while($row=mysqli_fetch_array($result, MYSQLI_ASSOC)) 
+
+###time, value, reading.locationID, sensorSpec.typeID, device.deviceType, location.comment
+for($i=0; $i < sizeof($readings); $i++) 
 { 
-	$date=gmdate("Y-m-d\ | H:i:s\ ", $row[Time]);
+	$row=$readings[$i]; 
+
+	$date=gmdate("Y-m-d\ | H:i:s\ ", $row["time"]);
 	echo"<tr> 
-	<td> $row[ReadingID] </td>
-	<td> $row[SensorID] </td>
-	<td> $date </td>   
-	<td> $row[Value] </td>
-	<td> $row[LocationID] </td> 
+	<td> $date </td> 
+	<td> $row[comment] </td>	
+	<td> $row[temperature] </td>
+	<td> $row[humidity] </td>		 
+	<td> $row[deviceType] </td>
 	</tr>";	
 } 
 
